6db75fa63158f765a9363f5cb01fe49141c03b62
Diana <diana.wanjiru@student.moringaschool.com>
2022-08-24 14:24:49 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querrySelector('form').addEventListener('submit', (e)=>{
+      e.preventDefault();
+      newTask(e.target.new-task.value)
+    })
+  
+  });
+  
+  function newTask (task){
+    let ul=document.getElementById('tasks')
+    let li = document.createElement('li')
+    ul.appendChild('li')
+    li.innerHTML= `${task}`
+    
+  
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent('x')
+    li.appendChild(btn)
+  
+  }
+   function handleDelete(e){
+    e.target.parentNode.remove()
+   }
 });
